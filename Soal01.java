9099808611f43d966c200d60bde5b949299231d3
Raffi-002 <raffiihasann@gmail.com>
2024-10-29 17:09:57 +0700

    coba

--- Soal01.java
+++ Soal01.java
@@ -2,5 +2,31 @@ public class Soal01 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
+		Scanner input = new Scanner (System.in);
+
+		int bilangan1 = input.nextLine();
+		int bilangan2 = input.nextline();
+		int bilangan3 = input.nextline();
+		int bilangan4 = input.nextline();
+		int bilangan5 = input.nextline();
+
+		if (bilangan1 > bilangan2) {
+		System.out.println("valid");
+	} else if (bilangan2 > bilangan1) {
+		
 	}
 }
+}
+	
+
+	
+
+
+
+
+
+
+
+		
+	
+
